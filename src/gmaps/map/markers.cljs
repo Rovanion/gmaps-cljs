63a84b2f41845566a04a86b275bc3d0da8ace014
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2016-02-29 18:17:08 +0100

    improved management of info windows

--- src/gmaps/map/markers.cljs
+++ src/gmaps/map/markers.cljs
@@ -4,12 +4,20 @@
             [markerclusterer]))
 
 (def ^:private markers (atom {}))
+(def ^:private info-windows (atom {}))
 
 (defn- create-infowindow
   [content]
   (let [cnt (clj->js {"content" content})
         infowindow (google.maps.InfoWindow. cnt)]
     infowindow))
+
+(defn- refresh-infowindow
+  [marker map-obj]
+  (let [infowindow (get @info-windows marker)]
+    (google.maps.event.clearInstanceListeners marker)
+    (.addListener marker "click" (fn []
+                                   (.open infowindow map-obj marker)))))
 
 (defn- create-marker
   [data map-obj]
@@ -18,7 +26,8 @@
     (when (contains? data :infowindow)
       (let [infowindow (create-infowindow (get data :infowindow))]
         (.addListener marker "click" (fn []
-                                       (.open infowindow map-obj marker)))))
+                                       (.open infowindow map-obj marker)))
+        (swap! info-windows assoc marker infowindow)))
     marker))
 
 (defn- create-clusterer
@@ -31,6 +40,10 @@
   (when-not (contains? @markers (get data :title))
     (swap! markers assoc (get data :title) (create-marker data map-obj)))
   (get @markers (get data :title)))
+
+(defn- get-infowindow
+  [marker]
+  (get @info-windows marker))
 
 (defn- setify [coll]
   (if (set? coll)
@@ -47,7 +60,9 @@
           (.setMap (get-marker md map-obj) nil))
         ;; Add new markers to map
         (doseq [md (s/difference new old)]
-          (.setMap (get-marker md map-obj) map-obj))))))
+          (let [marker (get-marker md map-obj)]
+            (.setMap marker map-obj)
+            (refresh-infowindow marker map-obj)))))))
 
 (defn update-markers-clusterer! [clusterer map-obj old new]
   (when-not (= old new)
@@ -56,7 +71,10 @@
       (when-not (= old new)
         ;; Remove markers no longer present
         (doseq [md (s/difference old new)]
-          (.removeMarker clusterer (get-marker md map-obj)))
+          (let [marker (get-marker md map-obj)]
+            (.removeMarker clusterer marker)))
         ;; Add new markers to clusterer
         (doseq [md (s/difference new old)]
-          (.addMarker clusterer (get-marker md map-obj)))))))
+          (let [marker (get-marker md map-obj)]
+            (.addMarker clusterer marker)
+            (refresh-infowindow marker map-obj)))))))
